78e5291391a334bef5c24d07e397c1eb29577a45
rennaNVM <onlyswitch07@gmail.com>
2026-09-22 11:54:25 -0300

    Nuevo formato para los for's y otros
    Se añadió una variable global que cuenta la cantidad de alumnos que hay en el archivo "Alumnos"; por lo tanto, se cambió el formato de todos los for para que funcione con esta nueva variable, además de añadir ciertos controles de errores y avanzar en el punto 4.

--- src/Talleres/TallerONE.java
+++ src/Talleres/TallerONE.java
@@ -1,3 +1,6 @@
+// Renato Argandoña 22290100-6 ICCI
+// Dominique Montiel 22325504-3 ICCI
+
 package Talleres;
 
 import java.io.File;
@@ -6,6 +9,8 @@ import java.util.Scanner;
 
 public class TallerONE {
 
+	static int cantidadAlumnos = 0;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -13,10 +18,12 @@ public class TallerONE {
 		String[][] datosSolicitudes = new String[0][0];
 		String[][] alumnoRegistrado = new String[50][4];
 		String[][] alumnoNoPertenece = new String[50][4];
+		String resp;
 		int cont = 0;
 		int contR = 0;
 		int res = 0;
 		int datosAux = 0;
+		int elec = 0;
 
 		do {
 			System.out.println("===== Sistema de Control del Grupo POO =====");
@@ -31,14 +38,19 @@ public class TallerONE {
 			res = Integer.parseInt(sc.nextLine());
 
 			if (res == 1) {
-				try {
-					datosAlumnos = cargarAlumnos();
-					datosSolicitudes = cargarSolicitudes();
-					System.out.println("Datos cargados");
+				if (datosAux >= 1) {
+					System.out.println("Los datos ya estan cargados!");
 					System.out.println("");
-					datosAux += 1;
-				} catch (Exception e) {
-					System.out.println("El archivo no existe");
+				} else {
+					try {
+						datosAlumnos = cargarAlumnos();
+						datosSolicitudes = cargarSolicitudes();
+						System.out.println("Datos cargados");
+						System.out.println("");
+						datosAux += 1;
+					} catch (Exception e) {
+						System.out.println("El archivo no existe");
+					}
 				}
 			}
 
@@ -49,7 +61,7 @@ public class TallerONE {
 				} else {
 					for (int i = 0; i < 50; i++) {
 						try {
-							if (datosSolicitudes[0][i].equals(null)) {
+							if (datosSolicitudes[i][0].equals(null)) {
 							}
 						} catch (Exception e) {
 							break;
@@ -57,30 +69,23 @@ public class TallerONE {
 
 						int aux = 1;
 
-						for (int o = 0; o < 50; o++) {
-							try {
-								if (datosAlumnos[0][o].equals(null)) {
-								}
-							} catch (Exception e) {
-								break;
-							}
-
-							boolean mismoNombre = datosSolicitudes[0][i].equals(datosAlumnos[0][o]);
-							boolean mismoApellido = datosSolicitudes[1][i].equals(datosAlumnos[1][o]);
+						for (int o = 0; o < cantidadAlumnos; o++) {
+							boolean mismoNombre = datosSolicitudes[i][0].equals(datosAlumnos[o][0]);
+							boolean mismoApellido = datosSolicitudes[i][1].equals(datosAlumnos[o][1]);
 
 							if (mismoNombre && mismoApellido) {
-								alumnoRegistrado[cont][0] = datosAlumnos[0][o];
-								alumnoRegistrado[cont][1] = datosAlumnos[1][o];
-								alumnoRegistrado[cont][2] = datosAlumnos[2][o];
-								alumnoRegistrado[cont][3] = datosAlumnos[3][o];
+								alumnoRegistrado[cont][0] = datosAlumnos[o][0];
+								alumnoRegistrado[cont][1] = datosAlumnos[o][1];
+								alumnoRegistrado[cont][2] = datosAlumnos[o][2];
+								alumnoRegistrado[cont][3] = datosAlumnos[o][3];
 								cont++;
 								aux -= 1;
 								break;
 							}
 						}
 						if (aux == 1) {
-							alumnoNoPertenece[contR][0] = datosSolicitudes[0][i];
-							alumnoNoPertenece[contR][1] = datosSolicitudes[1][i];
+							alumnoNoPertenece[contR][0] = datosSolicitudes[i][0];
+							alumnoNoPertenece[contR][1] = datosSolicitudes[i][1];
 							contR++;
 						}
 
@@ -102,7 +107,7 @@ public class TallerONE {
 				System.out.println("1) Por nombre completo");
 				System.out.println("2) Por rut");
 				System.out.print(">>");
-				int elec = Integer.parseInt(sc.nextLine());
+				elec = Integer.parseInt(sc.nextLine());
 
 				if (elec == 1) {
 					System.out.print("Ingrese el nombre completo: ");
@@ -112,7 +117,7 @@ public class TallerONE {
 					String rut = sc.nextLine();
 
 				}
-			} else if (res == 4) { //EN DESARROLLO
+			} else if (res == 4) { // EN DESARROLLO
 				if (datosAux == 0) {
 					System.out.println("No has cargados los datos de los alumnos!");
 					System.out.println("");
@@ -122,23 +127,51 @@ public class TallerONE {
 					System.out.println("2. Eliminar un alumno del curso");
 					System.out.println("3. Inscribir un alumno al curso");
 					System.out.println("4. Salir");
+					System.out.print(">> ");
 					res = Integer.parseInt(sc.nextLine());
 					if (res == 1) {
 						System.out.println("=== DATOS DE LOS ALUMNOS ===");
-						for (int i = 0; i < 50; i++) {
-							try {
-								if (datosSolicitudes[0][i].equals(null)) {
-								}
-							} catch (Exception e) {
-								break;
-							}
+						for (int i = 0; i < cantidadAlumnos; i++) {
 							System.out.printf("%s. ", (i + 1));
-							System.out.println(datosAlumnos[0][i] + " " + datosAlumnos[1][i] + " " + datosAlumnos[2][i]
-									+ " " + datosAlumnos[3][i]);
+							System.out.println(datosAlumnos[i][0] + " " + datosAlumnos[i][1] + " " + datosAlumnos[i][2]
+									+ " " + datosAlumnos[i][3]);
 
 						}
 						System.out.println("¿A quien deseas cambiar de paralelo? [Número]");
-						System.out.println(">> ");
+						System.out.print(">> ");
+						res = Integer.parseInt(sc.nextLine());
+
+						for (int i = 0; i < cantidadAlumnos; i++) {
+							if (res == i + 1) {
+								System.out.print("¿A que paralelo deseas cambiar a ");
+								System.out.println(datosAlumnos[i][0] + " " + datosAlumnos[i][1] + "? [C1 o C2]");
+								System.out.println("0. Salir");
+								System.out.print(">> ");
+								resp = sc.nextLine();
+								String[] partes = resp.split("");
+								do {
+									if (resp.equals("0")) {
+										break;
+									}
+									if (partes[1].equals("1")) {
+										datosAlumnos[i][3] = "C1";
+									} else if (partes[1].equals("2")) {
+										datosAlumnos[i][3] = "C2";
+									} else {
+										System.out.println("Paralelo no existente");
+										System.out.print("¿A que paralelo deseas cambiar a ");
+										System.out.println(datosAlumnos[i][0] + " " + datosAlumnos[i][1] + "? [C1 o C2]");
+										System.out.println("0. Salir");
+										System.out.print(">> ");
+										resp = sc.nextLine();
+										partes = resp.split("");
+									}
+								} while (!(partes[1].equals("1")) || !(partes[1].equals("2")) || resp.equals("0"));
+
+							}
+						}
+					}
+					if (res == 2) {
 
 					}
 				}
@@ -148,49 +181,46 @@ public class TallerONE {
 
 	}
 
-	private static String[][] cargarSolicitudes() throws IOException {
+	public static String[][] cargarSolicitudes() throws IOException {
 		File arch = new File("textos/Solicitudes.txt");
 		Scanner sFile = new Scanner(arch);
 
 		int o = 0;
-		String[] nombresSolicitados = new String[50];
-		String[] apellidosSolicitados = new String[50];
+		String[][] solicitudes = new String[50][2];
 
-		while (sFile.hasNextLine()) {
+		while (sFile.hasNextLine() && o < 50) {
 			String linea = sFile.nextLine();
 			String[] partes = linea.split("-");
-			nombresSolicitados[o] = partes[0];
-			apellidosSolicitados[o] = partes[1];
+			solicitudes[o][0] = partes[0];
+			solicitudes[o][1] = partes[1];
 			o++;
 		}
 
-		return new String[][] { nombresSolicitados, apellidosSolicitados };
+		return solicitudes;
 	}
 
-	private static String[][] cargarAlumnos() throws IOException {
+	public static String[][] cargarAlumnos() throws IOException {
 		File arch = new File("textos/Alumnos.txt");
 		Scanner sFile = new Scanner(arch);
 
 		int i = 0;
-		String[] nombres = new String[50];
-		String[] apellidos = new String[50];
-		String[] ruts = new String[50];
-		String[] paralelo = new String[50];
+		String[][] alumnos = new String[50][4];
 
 		while (sFile.hasNextLine() && i < 50) {
 			String linea = sFile.nextLine();
 			String[] partes = linea.split(";");
-			nombres[i] = partes[0];
-			apellidos[i] = partes[1];
-			ruts[i] = partes[2];
-			paralelo[i] = partes[3];
+			alumnos[i][0] = partes[0];
+			alumnos[i][1] = partes[1];
+			alumnos[i][2] = partes[2];
+			alumnos[i][3] = partes[3];
 			i++;
+			cantidadAlumnos += 1;
 		}
 		if (sFile.hasNextLine()) {
 			System.out.println("Te haz quedado sin espacio!");
-			return new String[][] { nombres, apellidos, ruts, paralelo };
+			return alumnos;
 		}
-		return new String[][] { nombres, apellidos, ruts, paralelo };
+		return alumnos;
 	}
 
 }
